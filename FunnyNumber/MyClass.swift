deb1b1d8da6efd1832a2798de2d15b4730c1f601
MasterUNG <phrombutr@gmail.com>
2018-06-03 14:14:49 +0700

    Home OK

---
+++ FunnyNumber/MyClass.swift
@@ -0,0 +1,40 @@
+//
+//  MyClass.swift
+//  FunnyNumber
+//
+//  Created by MasterUNG on 3/6/2561 BE.
+//  Copyright © 2561 MasterUNG. All rights reserved.
+//
+
+import Foundation
+
+class MyClass {
+    
+    
+    func decreseNumber(numberInt: Int) -> Int {
+        
+        let resultInt: Int = numberInt - 1
+        return resultInt
+        
+    
+    }
+    
+    func increaseNumber(numberInt: Int) -> Int {
+        return numberInt + 1
+        
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
+
+
+
+
+
--- FunnyNumber/ViewController.swift
+++ FunnyNumber/ViewController.swift
@@ -9,6 +9,66 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+//    Explicit
+    var myClass = MyClass()
+    
+    
+    
+//    Get Current Number
+    func getCurrentNumber() -> Int {
+        
+        let numberString: String = numberLabel.text!
+        let resultInt: Int = Int(numberString)!
+        return resultInt
+        
+    }
+    
+    func showLog(message: String) -> Void {
+        print("Message ==> \(message)")
+    }
+    
+    func showNumber(numberInt: Int) -> Void {
+        let numberString: String = String(numberInt)
+        numberLabel.text = numberString
+        
+    }
+    
+    
+    @IBOutlet weak var numberLabel: UILabel!
+    
+    
+    @IBAction func decButton(_ sender: Any) {
+        
+        let currentNumberInt: Int = getCurrentNumber()
+        showLog(message: "currentNumber ==> \(currentNumberInt)")
+        
+        var resultInt: Int = myClass.decreseNumber(numberInt: currentNumberInt)
+        showLog(message: "resultNumber = \(resultInt)")
+        
+        showNumber(numberInt: resultInt)
+        
+    }
+    
+    
+    
+    @IBAction func startButton(_ sender: Any) {
+        showNumber(numberInt: 1)
+    }
+    
+    
+    
+    @IBAction func incButton(_ sender: Any) {
+        
+        var currentNumberInt: Int = getCurrentNumber()
+        var numberInt: Int = myClass.increaseNumber(numberInt: currentNumberInt)
+        showNumber(numberInt: numberInt)
+        
+        
+        
+    }
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
